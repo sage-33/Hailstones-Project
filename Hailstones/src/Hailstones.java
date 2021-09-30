aef8d4b342d683f6262086cf8dfb84c0b5761767
Sage Silberman <ssilberman2023@chadwickschool.org>
2021-09-30 09:39:32 -0700

    style: Change comment format

--- Hailstones/src/Hailstones.java
+++ Hailstones/src/Hailstones.java
@@ -1,55 +1,68 @@
 
-import java.util.Scanner; //import scanner!!
+//import scanner!!
+import java.util.Scanner;
 
 public class Hailstones {
 	public static void main(String[] args) {
 
-		Scanner scanPos = new Scanner(System.in); // Create a Scanner object
+		// Create a Scanner object
+		Scanner scanPos = new Scanner(System.in);
 
 		System.out.println("Enter a positive hailstone starting value");
-		int posInt = scanPos.nextInt(); // posInt is going to be equal to the next integer that the system scans
+		// posInt is going to be equal to the next integer that the system scans
+		int posInt = scanPos.nextInt();
 
-		int initialNum = posInt; // to place hold the initial number (so not modified) so we can print it out at
-									// the end
-		int termCount = 1; // initialize how many we are starting with
+		// to place hold the initial number (so not modified) so we can print it out at
+		// the end
+		int initialNum = posInt;
+
+		// initialize how many we are starting with
+		int termCount = 1;
 		int biggest = 0;
 
 		System.out.println(" ");
-		System.out.println(posInt); // enter the pos
+		// print the pos
+		System.out.println(posInt);
 
-		while (posInt != 1) // while the positive integer is not 1
-		{
-			if (posInt % 2 == 0) // if the remainder of pos int is 0 (meaning its even)
-			{
-				posInt = posInt / 2; // if its even then it'll be divided by 2
+		// while the positive integer is not 1
+		while (posInt != 1) {
+			// if the remainder of pos int is 0 (meaning its even)
+			if (posInt % 2 == 0) {
+				// if its even then it'll be divided by 2
+				posInt = posInt / 2;
 				System.out.println(posInt);
-				termCount++; // counts the terms
+				// counts the terms
+				termCount++;
 
 				if (posInt > biggest) {
-					biggest = posInt; // place holds the biggest number
+					// place holds the biggest number
+					biggest = posInt;
 				}
 
 			}
 
-			else if (posInt % 2 != 0) // if the remainder of positive integer doesn't equal 0
-			{
+			// if the remainder of positive integer doesn't equal 0
+			else if (posInt % 2 != 0) {
 				posInt = posInt * 3;
-				posInt = posInt + 1; // the math stuff
+				// the math stuff
+				posInt = posInt + 1;
 				System.out.println(posInt);
-				termCount++; // counts the terms
+				// counts the terms
+				termCount++;
 
 				if (posInt > biggest) {
-					biggest = posInt; // place holds the biggest number
+					// place holds the biggest number
+					biggest = posInt;
 				}
 
 			}
 
 		}
 
+		// out prints all the start,term count, and biggest number
 		System.out.println("start: " + initialNum);
 		System.out.println("term count: " + termCount);
 		System.out.println("biggest: " + biggest);
-		// out prints all the start,term count, and biggest number
 
 	}
 
